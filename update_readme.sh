26fe90b638fd078f5fa5b2a9805ed0828dfb4622
iphren <contact@iphren.com>
2023-02-14 21:33:47 +0000

    Update Pastor Cheng's info

--- update_readme.sh
+++ update_readme.sh
@@ -4,7 +4,7 @@ for name in $(ls speakers)
 do
     cd speakers/${name}
     perl -i -0777 -pe 's/^\[\/\/\]: # \(photos\).*/[\/\/]: # (photos)\n/sm' README.md
-    for pic in $(ls *.jpeg 2> /dev/null)
+    for pic in $(ls *.jpeg *.jpg *.png 2> /dev/null)
     do
         sed -i "\$s/$/\n![${pic%.*}]($pic)/" README.md
     done
